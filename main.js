239ba55ace9cda56d837eeb94cd5190657a94775
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2021-05-26 12:44:45 +0300

    Update main.js
    gallery added

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var dampc = {}
+
 dampc.feedbackForm = document.getElementById('FeedbackForm')
 dampc.feedbackBT = document.getElementById('FeedbackBT')
 dampc.feedbackTextList = document.querySelectorAll('.FeedbackTextInputGroup')
 dampc.feedbackTextarea = dampc.feedbackTextList[0].querySelector('textarea')
 dampc.feedbackInputName = dampc.feedbackTextList[1].querySelector('input')
 dampc.feedbackInputEmail = dampc.feedbackTextList[2].querySelector('input')
+
+dampc.galleryThumbs = document.getElementById('GalleryGroup')
+dampc.galleryFullsize = document.getElementById('Gallery_Fullsize')
 
 //-----------------footer: dynamic year-----------------
 
@@ -25,8 +29,16 @@ dampc.feedbackInputOnClick = function(node) {
     dampc.feedbackInputName.style.backgroundColor = '#fff'
     dampc.feedbackInputEmail.style.backgroundColor = '#fff'
     node.style.backgroundColor = '#fde6ea'
-}
+};
 dampc.feedbackTextarea.addEventListener('click',  function(event) {dampc.feedbackInputOnClick (dampc.feedbackTextarea)})
 dampc.feedbackInputName.addEventListener('click',  function(event) {dampc.feedbackInputOnClick (dampc.feedbackInputName)})
 dampc.feedbackInputEmail.addEventListener('click',  function(event) {dampc.feedbackInputOnClick (dampc.feedbackInputEmail)})
+
+//-----------------Gallery_Section: thumbnails and fullscreen images
+
+dampc.galleryThumbs.addEventListener("click", function(event) {
+    dampc.galleryFilename = event.target.src.split("/").pop()
+    dampc.galleryFullsize.src = "img_gallery/fullsize/" + dampc.galleryFilename
+    dampc.galleryFullsize.alt = event.target.alt
+})
 
