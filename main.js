542223b008df48877467945a1e58092789e8c6bd
Olga <53094440+OlgaOmmm@users.noreply.github.com>
2021-05-26 11:20:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,20 +13,20 @@ document.getElementById("FooterYear_Sub").innerHTML = new Date().getFullYear();
 //-----------------Feedback_Section: FeedbackForm.submit() event; .FeedbackTextInputGroup event
 
 //FeedbackForm.submit() event:
-dampc.feedbackBT.addEventListener('click', (event)=> {dampc.feedbackForm.submit()})
+dampc.feedbackBT.addEventListener('click', function(event) {dampc.feedbackForm.submit()})
 
 //.FeedbackTextInputGroup event:
 dampc.feedbackTextarea.title = 'Please input your message'
 dampc.feedbackInputName.title = 'Please input your name'
 dampc.feedbackInputEmail.title = 'Please input your email'
-dampc.TextboxOnClick = function (node) {
+dampc.feedbackInputOnClick = function(node) {
     node.placeholder = ''
     dampc.feedbackTextarea.style.backgroundColor = '#fff'
     dampc.feedbackInputName.style.backgroundColor = '#fff'
     dampc.feedbackInputEmail.style.backgroundColor = '#fff'
     node.style.backgroundColor = '#fde6ea'
 }
-dampc.feedbackTextarea.addEventListener('click',  (event) => {dampc.TextboxOnClick (dampc.feedbackTextarea)})
-dampc.feedbackInputName.addEventListener('click',  (event) => {dampc.TextboxOnClick (dampc.feedbackInputName)})
-dampc.feedbackInputEmail.addEventListener('click',  (event) => {dampc.TextboxOnClick (dampc.feedbackInputEmail)})
+dampc.feedbackTextarea.addEventListener('click',  function(event) {dampc.feedbackInputOnClick (dampc.feedbackTextarea)})
+dampc.feedbackInputName.addEventListener('click',  function(event) {dampc.feedbackInputOnClick (dampc.feedbackInputName)})
+dampc.feedbackInputEmail.addEventListener('click',  function(event) {dampc.feedbackInputOnClick (dampc.feedbackInputEmail)})
 
